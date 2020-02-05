791626aa5992211e3f219cc3c756b2892c832fa5
An Rodriguez <anmichel.rodriguez@annalect.com>
2020-02-05 12:36:56 -0500

    implementing deletion of bucket contents; bugfix when handling exception

--- lib/modules/aws_s3.py
+++ lib/modules/aws_s3.py
@@ -11,6 +11,7 @@ class Resource(BaseResource):
 
     def __init__(self):
         self.client = boto3.client(self.type)
+        self.resource = boto3.resource(self.type)
 
     def get_resources(self):
         """ Gets list of S3 buckets """
@@ -30,16 +31,24 @@ class Resource(BaseResource):
     def list_resources(self):
         pprint(self.resources)
 
+    def delete_bucket_contents(self, bucket):
+        if self.check_dry_run():
+            return True
+        bucket = self.resource.Bucket(bucket)
+        bucket.objects.all().delete()
+        return True
+
     def delete_resources(self, resources, options={}):
         for resource in resources:
             bucket_name = resource['Name']
-            print(f"Delete {bucket_name}")
             try:
-                response = self.client.delete_bucket(Bucket=bucket_name)
+                if options['force']:
+                    self.delete_bucket_contents(bucket_name)
+                self.client.delete_bucket(Bucket=bucket_name)
+                print(f"Bucket {bucket_name} deleted.")
             except Exception as error:
-                if error.response['Error']['Code'] == 'BucketNotEmpty':
-                    if options['force']:
-                        print('WIP: delete bucket contents first.')
-                    else:
-                        warning.warn('Bucket not empty and force flag not set.')
+                if hasattr(error, 'response') and error.response['Error']['Code'] == 'BucketNotEmpty':
+                    warning.warn('Bucket not empty and force flag not set.')
+                else:
+                    raise(error)
 
